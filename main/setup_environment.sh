c55424905b5265a038160d8b0c6f4174c4a2b1b8
Chanuka Fernando <160318713+ch9nuk9@users.noreply.github.com>
2024-05-22 11:53:16 +0200

    Update setup_environment.sh

--- main/setup_environment.sh
+++ main/setup_environment.sh
@@ -9,7 +9,7 @@ source activate NTF
 # Install dependencies
 conda install pandas numpy matplotlib scipy tk -y
 
-echo "Environment setup complete. To activate the environment, use 'conda activate tracking_env'."
+echo "Environment setup complete. To activate the environment, use 'conda activate NTF'."
 
 # Use the following line of code to run this script prior running the NTF_main.py (Fresh setups only)
 # bash setup_environment.sh
