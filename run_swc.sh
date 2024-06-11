d9ff8bdf796b2179b5c0dc287936dc3dae829da3
Chanuka Fernando <160318713+ch9nuk9@users.noreply.github.com>
2024-06-11 10:59:30 +0200

    Add files via upload
    Signed-off-by: Chanuka Fernando <160318713+ch9nuk9@users.noreply.github.com>

---
+++ run_swc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the conda environment
+source activate SWC
+
+# Change to the specified directory
+cd /mnt/c/GitHub_repositorys/SimpleWormCropper
+
+# Run the Python script
+python SWC_main_gui.py
